34c8048c0a43815872dd9f75dc969815a10057ed
Virgile Robles <virgile.robles@protonmail.ch>
2018-12-03 00:33:46 +0100

    APU cassé

--- nes.ml
+++ nes.ml
@@ -2,6 +2,7 @@ open Rom_loader
 
 module NesCpu = Cpu.Make (struct
     let is_in_ppu_range addr = (addr lsr 13) = 1
+    let is_in_apu_range addr = addr >= 0x4000 && addr <= 0x4017
     let read mem a =
         if is_in_ppu_range a then
             Ppu.get_register a
@@ -13,6 +14,8 @@ module NesCpu = Cpu.Make (struct
             Ppu.set_register a v
         else if a = 0x4014 then
             Ppu.dma mem (v lsl 8)
+        else if is_in_apu_range a then
+            Apu.write_register v a
         else mem.(a) <- v
 end)
 
@@ -29,18 +32,24 @@ let rec ppu_exec_n_cycles n =
         ppu_exec_n_cycles (n - 1)
     )
 
-let rec main_loop frame limit =
+let rec n_times f n =
+    if n > 0 then (
+        f (); n_times f (n - 1)
+    )
+
+let rec main_loop frame limit sup_cycle =
     Input.get_inputs ();
     if frame != limit && (Input.continue ()) then (
 (*         NesCpu.print_state (); *)
         let old = !NesCpu.cycle_count in
         NesCpu.fetch_instr ();
         let elapsed = !NesCpu.cycle_count - old in
-        ppu_exec_n_cycles (elapsed * 3);
-        main_loop (frame + 1) limit
+        n_times Apu.next_cycle ((elapsed + sup_cycle) / 2);
+        n_times Ppu.next_cycle (elapsed * 3);
+        main_loop (frame + 1) limit (elapsed mod 2)
     )
 
-let start_main_loop = main_loop 0
+let start_main_loop lim = main_loop 0 lim 0
 
 let main =
     if Array.length Sys.argv > 1 then (
@@ -50,7 +59,9 @@ let main =
         NesCpu.stack_pointer := 0xFD ;
         NesCpu.processor_status := 0x34 ;
         NesCpu.program_counter := (NesCpu.memory.(0xFFFD) lsl 8) lor NesCpu.memory.(0xFFFC) ;
+        Apu.init ();
         start_main_loop (-1);
-        Ppu.exit ()
+        Ppu.exit ();
+        Apu.exit ()
     )
     else Printf.printf "No ROM provided\n"
