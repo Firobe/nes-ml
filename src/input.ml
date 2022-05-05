5c58f898618bfd83f9ca8d21ac7dd42e385a0926
Virgile Robles <virgile.robles@protonmail.ch>
2022-05-05 18:15:12 +0200

    Pump events

--- src/input.ml
+++ src/input.ml
@@ -37,10 +37,7 @@ let key_pressed key =
 let continue () =
   state.{Sdl.Scancode.escape} == 0
 
-let rec get_inputs () =
-  let event = Sdl.Event.create () in
-  if Sdl.poll_event (Some event) then
-    get_inputs ()
+let get_inputs () = Sdl.pump_events ()
 
 type t = {mutable next_key : int}
 let create () = {next_key = 0}
